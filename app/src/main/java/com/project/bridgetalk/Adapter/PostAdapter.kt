97c2b615138b4d4782a53337c6d0764c3946137b
KoEunsuk <koeunsuk12@gmail.com>
2024-12-12 06:37:20 +0900

    마이페이지 UI - 버튼수정

--- app/src/main/java/com/project/bridgetalk/Adapter/PostAdapter.kt
+++ app/src/main/java/com/project/bridgetalk/Adapter/PostAdapter.kt
@@ -3,7 +3,7 @@ package com.project.bridgetalk.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.project.bridgetalk.R
@@ -17,14 +17,14 @@ class PostAdapter(private val posts: List<Post>, private val listener: OnPostCli
         private val contentTextView: TextView = itemView.findViewById(R.id.content)
         private val likeCountTextView: TextView = itemView.findViewById(R.id.likeCount)
         private val createdAtTextView: TextView = itemView.findViewById(R.id.createdAt)
-        private val editButton: Button = itemView.findViewById(R.id.editButton)
-        private val deleteButton: Button = itemView.findViewById(R.id.deleteButton)
+        private val editButton: ImageButton = itemView.findViewById(R.id.editButton)
+        private val deleteButton: ImageButton = itemView.findViewById(R.id.deleteButton)
 
         fun bind(post: Post) {
             titleTextView.text = post.title
             contentTextView.text = post.content
             likeCountTextView.text = post.like_count.toString()
-            createdAtTextView.text = post.createdAt
+            createdAtTextView.text = post.updatedAt
 
             editButton.setOnClickListener {
                 listener.onEditClick(post)
